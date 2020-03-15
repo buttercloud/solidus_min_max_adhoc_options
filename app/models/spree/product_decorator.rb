b1bff1f038b222c18541b09c40829e5478ec5c3f
Islam Odeh <islamodeh@hotmail.com>
2020-03-15 22:54:29 +0200

    Update product available function

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
   def available?
-    (!(available_on.nil? || available_on.future?) && !deleted?) && stock_items.any?(&:not_available)
+    (!(available_on.nil? || available_on.future?) && !deleted?) && !stock_items.any?(&:not_available)
   end
 end
